42994994fa881e74c50f62ae09cf8f52a89b8acc
GustavoIgn <gustavoignacio280@gmail.com>
2023-12-29 15:53:27 -0300

    Desafio Resolvido - GSI

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,8 +6,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\nDigite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
@@ -23,35 +22,26 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("Digite a sua opção: \n1 - Cadastrar Veículo\n2 - Remover Veículo\n3 - Listar Veículos\n4 - Encerrar");
 
     switch (Console.ReadLine())
     {
         case "1":
             es.AdicionarVeiculo();
             break;
-
         case "2":
             es.RemoverVeiculo();
             break;
-
         case "3":
             es.ListarVeiculos();
             break;
-
         case "4":
             exibirMenu = false;
             break;
-
         default:
             Console.WriteLine("Opção inválida");
             break;
     }
-
     Console.WriteLine("Pressione uma tecla para continuar");
     Console.ReadLine();
 }
